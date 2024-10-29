6a965ded842b23fd2282184690005867e13a6a71
Fareed95 <fareedsayed95@gmail.com>
2024-10-29 18:56:30 +0530

    updated the docker and containerized

--- precommit_checking.sh
+++ precommit_checking.sh
@@ -1,3 +1,12 @@
+# Create a Python virtual environment (recommended to avoid PEP 668 issue)
+if [ ! -d "venv" ]; then
+    echo "Creating a Python virtual environment..."
+    python3 -m venv venv
+fi
+
+# Activate virtual environment
+echo "Activating the virtual environment..."
+. ./venv/bin/activate
 
 echo "Installing detect secret to initialize pre commit hook ..."
 pip install detect-secrets
