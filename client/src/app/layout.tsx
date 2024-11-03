947557101aa156adf0daeb81c52c62f9069d0be6
Rehbar Khan <136853370+thisisarsh1@users.noreply.github.com>
2024-11-03 16:11:17 +0530

    deployment issue sorted

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -22,21 +22,18 @@ export const metadata: Metadata = {
   description: "Code Cell RCOE",
 };
 
-
-
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-
-  
   return (
     <html lang="en">
-      
+      {/* <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png"/>
+      </head> */}
       
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,59 +4,11 @@ import { useEffect } from 'react';
 import Herotext from '@/components/Herotext';
 import HomeEventCard from '@/components/HomeEventCard';
 import Footer from '@/components/Footer';
-import { useUserContext } from '@/app/context/Userinfo';
-import { useToast } from "@/hooks/use-toast"
+
 
 
 export default function Home() {
-  
-  const {contextsetIsLoggedIn,contextsetEmail,contextsetName,contextisLoggedIn}= useUserContext();
 
-  const { toast } = useToast();
-
-  const getUserInfo = async () => {
-    const token = localStorage.getItem('authToken');
-    if (!token) return; // Early return if no token exists
-
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user`, {
-        method: 'GET',
-        headers: {
-          "Authorization": token,
-          'Content-Type': "application/json",
-        },
-        credentials: 'include',
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch user info');
-      }
-
-      const result = await response.json();
-      contextsetIsLoggedIn(true);
-      contextsetEmail(result.email);
-      contextsetName(result.name);
-      toast({
-        title: "Successfully logged in",
-        description: `Welcome back, ${result.name}!`,
-      });
-
-    } catch (error) {
-      console.error("Error fetching user info:", error);
-      toast({
-        title: "Error",
-        description: "Failed to fetch user information",
-        variant: "destructive",
-      });
-    }
-  };
-
-  useEffect(() => {
-    if(contextisLoggedIn==false){
-        getUserInfo()
-    }
-    
-  },[contextisLoggedIn])
   return (
     <div className="min-h-screen">
       <Herotext />
@@ -64,4 +16,5 @@ export default function Home() {
       <Footer />
     </div>
   );
+  
 }
