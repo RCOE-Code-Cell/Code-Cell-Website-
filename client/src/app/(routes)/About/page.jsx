d4ade32963d06325eb5181312d091f99b2352576
Rehbar Khan <136853370+thisisarsh1@users.noreply.github.com>
2024-11-04 17:49:22 +0530

    few changes

--- client/src/app/(routes)/About/page.jsx
+++ client/src/app/(routes)/About/page.jsx
@@ -9,7 +9,7 @@ function page() {
         className="-top-45 left-5 sm:left-60 sm:-top-20"
         fill="white"
       />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 mt-10">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           ABOUT US 
         </h1>
