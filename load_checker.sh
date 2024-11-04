f019d64bf00c888bc538109970d3efa3998dcd11
Fareed95 <fareedsayed95@gmail.com>
2024-11-04 12:28:56 +0530

    updated the load checker

--- load_checker.sh
+++ load_checker.sh
@@ -2,7 +2,7 @@
 
 # Set the URLs for your apps
 DJANGO_URL="https://code-cell-website-fv8m.onrender.com/"
-# NEXT_URL="https://nutriscann.vercel.app/"
+NEXT_URL="https://code-cell-website.vercel.app/"
 
 # Number of requests
 REQUESTS=100
@@ -12,5 +12,5 @@ CONCURRENCY=10
 echo "Testing Django App..."
 ab -n $REQUESTS -c $CONCURRENCY $DJANGO_URL
 
-# echo "Testing Next.js App..."
-# ab -n $REQUESTS -c $CONCURRENCY $NEXT_URL
+echo "Testing Next.js App..."
+ab -n $REQUESTS -c $CONCURRENCY $NEXT_URL
