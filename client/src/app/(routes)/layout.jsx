0ad881dc073d8ebbc75dda390eab6796bd1c0c6c
Rehbar Khan <136853370+thisisarsh1@users.noreply.github.com>
2024-11-07 16:03:18 +0530

    Update layout.jsx

--- client/src/app/(routes)/layout.jsx
+++ client/src/app/(routes)/layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
         <div>
            
             <main>
-              <GetUserInfo/>
+              {/* <GetUserInfo/> */}
                 {children}
             </main>
             <footer>
