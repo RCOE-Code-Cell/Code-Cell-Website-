66c7b8eb6a79c4aa1739568ae08ece03d2afd64c
Rehbar Khan <136853370+thisisarsh1@users.noreply.github.com>
2024-11-22 00:00:37 +0530

    updated responsiveness of OTP

--- client/src/components/OTP.jsx
+++ client/src/components/OTP.jsx
@@ -135,7 +135,7 @@ const Getuserinfo = async () => {
   <form  onSubmit={handleSubmit}>
     <div className="flex items-center justify-center min-h-screen ">
 
-      <div className="p-4 bg-black shadow-md rounded-xl border border-neutral-500 border-dashed w-[30%] h-[50vh] flex  flex-col items-center justify-center">
+      <div className="p-4 bg-black shadow-md rounded-xl border border-neutral-500 border-dashed sm:w-[30%] w-[90%] h-[50vh] flex  flex-col items-center justify-center">
     <div className='text-neutral-500 font-bold text-xl m-4'>
         {`The OTP has been sent to  ${contextemail}  Please enter the OTP below`}
     </div>
@@ -157,7 +157,7 @@ const Getuserinfo = async () => {
 
 
       <button
-        className="relative group/btn flex items-center justify-center px-4 w-[50%] text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+        className="relative group/btn flex items-center justify-center px-4 sm:w-[50%] text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
         type="Submit"
       >
         <span className="text-neutral-700 dark:text-neutral-300 text-sm">
